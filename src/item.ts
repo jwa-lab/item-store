470e275c926c1cfebaa184633bd1d5cf2d39de36
Maïssa <maissa@maissanouhaud@outlook.fr>
2021-02-23 12:42:08 +0100

    JPS-10: Add name field to item
    - Add name field to item

--- src/item.ts
+++ src/item.ts
@@ -1,6 +1,7 @@
 export interface JSONItem {
     no_update_after: string | undefined;
     item_id: number;
+    name: string;
     data: { [k: string]: string };
     quantity: number;
     [key: string]: unknown;
